2ae000c1416ef196825fddb8fb9d3b78b45ba2e5
Jack Morrison <jackmorrison1@gmail.com>
2013-12-09 10:13:08 -0500

    New jonquil.sh

--- jonquil.sh
+++ jonquil.sh
@@ -2,4 +2,4 @@
 
 SETS="~/Dropbox/LearningNorth/JonquilFarm/2013.11.28_12:57:04/ ~/Dropbox/LearningNorth/JonquilFarm/2013.11.28_13:01:49/ ~/Dropbox/LearningNorth/JonquilFarm/2013.11.28_13:06:58/ ~/Dropbox/LearningNorth/JonquilFarm/2013.11.28_12:59:40/ ~/Dropbox/LearningNorth/JonquilFarm/2013.11.28_13:03:59/"
 
-python learning_north.py --lasso -data $SETS
+python learning_north.py --all -data $SETS -outdir jonquil
